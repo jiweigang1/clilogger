02aa80d95821bc9cd4b545b45da859bccf53d3b8
冀刚 <jiweigang2008@126.com>
2025-09-28 17:34:18 +0800

    index

---
+++ config.js
@@ -0,0 +1,45 @@
+import os   from 'os';
+import path from 'path';
+import fs from 'fs';
+import readline from 'readline';
+
+let defaultConfig = {
+    "deepseek":{
+        enable:false,
+        evn:{
+
+        }
+    },
+    "kimi-k2":{
+        enable:false,
+         evn:{
+            
+        }
+    },
+    "openrouter":{
+         enable:false,
+          evn:{
+            
+        }
+    }
+}
+
+function getConfigDir(){
+  let home = os.homedir();
+  return path.join(home,[".clilogger","config.json"]);
+}
+
+/**
+ * init config dir
+ */
+export function initConfig(){
+  //如果路径不存在，创建    
+  f (!fs.existsSync(getConfigDir())){
+      //创建初始化文件
+      fs.writeFileSync(filePath, JSON.stringify(defaultConfig));
+  } 
+}
+export function loadConfig(){
+   const data = fs.readFileSync(getConfigDir(), 'utf-8');
+   return JSON.parse(data);
+}
---
+++ uclaude.js
@@ -0,0 +1,55 @@
+import {initConfig,loadConfig} from "config"
+
+/**
+ * 启动 calude code
+ */
+function start(){
+   initConfig();
+   let allConfig = loadConfig();
+  
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    // 提供选项并让用户选择
+    rl.question('选择模型：1、deepseek ， 2、kimi、 3、openrouter', (answer) => {
+        // 关闭接口
+        rl.close();
+        // 根据用户的输入判断选择
+        var config = allConfig["deepseek"];
+        let env =  config.env;
+        const stdioConfig = config.NON_INTERACTIVE_MODE
+            ? ["pipe", "inherit", "inherit"] // Pipe stdin for non-interactive
+            : "inherit"; // Default inherited behavior
+        const claudeProcess = spawn(
+            “ --import /Users/jigang/Javaworks/clilogger/clogger.js ” + claudePath,
+            [],
+            {
+            env,
+            stdio: stdioConfig,
+            shell: true,
+            }
+        );
+
+        // Close stdin for non-interactive mode
+        if (config.NON_INTERACTIVE_MODE) {
+            claudeProcess.stdin?.end();
+        }
+
+        claudeProcess.on("error", (error) => {
+            console.error("Failed to start claude command:", error.message);
+            console.log(
+            "Make sure Claude Code is installed: npm install -g @anthropic-ai/claude-code"
+            );
+            decrementReferenceCount();
+            process.exit(1);
+        });
+
+        claudeProcess.on("close", (code) => {
+            decrementReferenceCount();
+            closeService();
+            process.exit(code || 0);
+        });
+    });
+}
