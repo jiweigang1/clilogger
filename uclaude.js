a977e1b704679c378b14be2dd816a9c74b58794f
冀刚 <jiweigang2008@126.com>
2025-09-28 18:20:01 +0800

    index

--- uclaude.js
+++ uclaude.js
@@ -16,7 +16,8 @@ function start(){
     });
 
     // 提供选项并让用户选择
-    rl.question('选择模型：1、deepseek ， 2、kimi、 3、openrouter', (answer) => {
+    rl.question("选择模型：1、deepseek ， 2、kimi、 3、openrouter", (answer) => {
+        console.log(allConfig);
         // 根据用户的输入判断选择
         var config = allConfig["deepseek"];
         let env =  config.env;
